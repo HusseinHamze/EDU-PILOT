485587b366ef10d984ffbdd002296eb1a21ba34b
Ali kabalan <alikabalan311@gmail.com>
2025-03-28 15:52:55 +0200

    Adjusted Login

---
+++ src/Home-Page/Home-Body/HomeBody.jsx
@@ -0,0 +1,20 @@
+import './homeBody.css'
+
+function HomeBody(){
+    
+    return(
+        <div className="body-container">
+            <h2>Not Sure What To Study? Let EduPilot Guide You!</h2>
+            
+            <button>Start Your Assessment</button>
+
+            <p>Answer a few questions and get personalized 
+                university major recommendations based on 
+                 interests and skills.</p>
+
+        </div>
+    );
+
+}
+
+export default HomeBody
---
+++ src/Home-Page/Home.jsx
@@ -0,0 +1,15 @@
+import HomeNavbar from "./Home-Nav/HomeNavbar"
+import HomeBody from "./Home-Body/HomeBody"
+import HomeElements from "./Home-Elements/HomeElements"
+
+function Home(){
+    return(
+        <>
+        <HomeNavbar/>
+        <HomeBody/>
+        <HomeElements/>
+        </>
+    )
+}
+
+export default Home
